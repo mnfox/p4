ab2b8d3e02c7d9ab7f590cd0a86757371d8a9b73
mnfox <mithila.fox@gmail.com>
2014-12-13 18:12:46 +1000

    tweaking db settings

--- app/config/database.php
+++ app/config/database.php
@@ -55,8 +55,8 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => '127.0.0.1',
-			'database'  => 'forge',
-			'username'  => 'forge',
+			'database'  => 'berry',
+			'username'  => 'root',
 			'password'  => 'CjCmOcjTNQ',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
