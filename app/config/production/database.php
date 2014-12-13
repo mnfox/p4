892b52ff98af43011ad2fdb6239446ed63264b83
mnfox <mithila.fox@gmail.com>
2014-12-13 17:52:09 +1000

    tweaking db settin

--- app/config/production/database.php
+++ app/config/production/database.php
@@ -7,6 +7,7 @@ return array(
         'mysql' => array(
             'driver'    => 'mysql',
             'host'      => '127.0.0.1',
+            'unix_socket'   => '/Applications/MAMP/tmp/mysql/mysql.sock',
             'database'  => 'berry',
             'username'  => 'root',
             'password'  => 'CjCmOcjTNQ',
